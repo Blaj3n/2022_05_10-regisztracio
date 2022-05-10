a87695410eb909d7522a9d72a9dcbe6309d77488
Blaj3n <ulrichbence15@freemail.hu>
2022-05-10 10:39:26 +0200

    órai munkai kiegészítés és javítás

--- class.user.php
+++ class.user.php
@@ -14,33 +14,28 @@
 				echo "Hibás csatlakozás.";
 				exit();
 			}
-			else echo "Sikeres kapcsolódás.";
+			else echo "Sikeres csatlakozás.";
 		}
 
 		/* regisztráció*/
-		/* azonosító automatikus, jogAzon default 2 lesz, csak a többit adja meg a felhasználó */
-		public function reg_felhasznalo($vNev, $kNev, $nev, $szulDatum, $jelszo, $email){
+		/* azonosító automatikus, jogosultsag!! default 2 lesz, csak a többit adja meg a felhasználó */
+		public function reg_felhasznalo($vNev, $kNev,$nev, $szulDatum, $jelszo, $email){
 			/* titkosítás */
 			$jelszo = md5($jelszo);
 			/* lekérdezés szövege, létezik-e ilyen nevű vagy e-mail című felhasználó? */
-			$sql = "SELECT * FROM felhasználók where nev='$nev' OR email='$email'";
-			//lekérdezés végrehajtása, eredménye egy php tömb
+			$sql = "SELECT * from felhasználók where nev='$nev' OR email='$email'";
+			//lekérdezés végrehajtása, eredménye phptömb
 			$letezik = $this->kapcsolat->query($sql);
 			//sorok száma lekérdezése
 			$sorokSzama = $letezik->num_rows;
 			//ha 0 az eredmény, akkor ilyen még nincs, felvesszük
-			if($sorokSzama == 0){
-				$sql1 = "INSERT INTO felhasználók SET
-				jogosultsag = 2, nev = '$nev',
-				jelszo = '$jelszo', email='$email',
-				vezetekNev = '$vNev',keresztNev = '$kNev',
-				szulDatum = '$szulDatum'";
+			if ($sorokSzama == 0){
+				$sql1 = "INSERT INTO felhasználók SET jogosultsag  = 2, nev = '$nev', jelszo = '$jelszo', email='$email', vezetekNev = '$vNev', keresztNev = '$kNev', szulDatum = '$szulDatum'";
 				//logikai típussal tér vissza
-				$eredmeny = mysqli_query($this->kapcsolat, $sql) or die(mysqli_connect_errno()."Nem illeszthető be az adat.");
+				$eredmeny = mysqli_query($this->kapcsolat, $sql1) or die(mysqli_connect_errno()."Nem illeszthető be az adat.");
 				return $eredmeny;
 			}
-			else
-			{
+			else{
 				return false;
 			}
 		}
@@ -48,25 +43,24 @@
 		/* bejelentkezés */
 		public function bejelentkezes($emailVagyNev, $jelszo){
 			//titkosítás
+			$jelszo = md5($jelszo);
 			//lekérdezés arra, hogy van-e ilyen név vagy email a táblában
-			$sql2 = "SELECT felhAzon FROM felhasználók where nev='$emailVagyNev' OR email='$emailVagyNev' and jelszo='$jelszo'";
+			$sql2 = "SELECT felhAzon from felhasználók where nev='$emailVagyNev' OR email='$emailVagyNev' and jelszo = '$jelszo'";
 			//sql lekérdezés végrehajtása, eredménye php tömb
 			$eredmeny = mysqli_query($this->kapcsolat, $sql2);
 			//sorok száma lekérdezése
 			$sorokSzama = $eredmeny->num_rows;
 			//php tömb átkonvertálása
 			$fAdat = mysqli_fetch_array($eredmeny);
-			//ha a sorok száma 1, akkor van ilyen
+			// ha a sorok száma 1, akkor van ilyen
 			if ($sorokSzama == 1){
 				$_SESSION['login'] = true;
 				$_SESSION['felhAzon'] = $fAdat['felhAzon'];
 				return true;
 			}
-			else
-			{
+			else{
 				return false;
 			}
-			
 		}
 
 		/* név megmutatása*/
--- registration.php
+++ registration.php
@@ -1,65 +1,52 @@
 <?php
-include_once 'class.user.php';
+include_once 'class.user.php';  
 
-$felh = new User(); // Checking for user logged in or not
+    $felh = new User(); // Checking for user logged in or not
 
-if (isset($_REQUEST['submit'])) {
-    extract($_REQUEST);
-    $register = $felh->reg_felhasznalo(
-        $vNev,
-        $kNev,
-        $nev,
-        $szulDatum,
-        $jelszo,
-        $email
-    );
-    if ($register) {
+    if (isset($_REQUEST['submit'])){
+        extract($_REQUEST);
+        $register = $felh->reg_felhasznalo($vNev,$kNev,$nev, $szulDatum,$jelszo,$email);
+        if ($register) {
         // sikeres regisztráció
-        echo 'Sikeres regisztráció <a href="login.php">Kattints ide!</a> a belépéshez';
-    } else {
+            echo 'Sikeres regisztráció <a href="login.php">Kattints ide!</a> a belépéshez';
+        } else {
         // sikertelen regisztráció
-        echo 'Sikertelen regisztráció. Email vagy felhasználónév már létezik. Próbáld újra!';
+            echo 'Sikertelen regisztráció. Email vagy felhasználónév már létezik. Próbáld újra!';
+        }
     }
-}
 ?>
 
 <!DOCTYPE html>
 <html lang="hu-HU">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Regisztráció</title>
-
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Regisztráció</title>
+        
     <style>
-        #container {
-            width: 400px;
-            margin: 0 auto;
-        }
+        #container{width:400px; margin: 0 auto;}
     </style>
-</head>
-
-<body>
-    <div id="container">
-        <h1>Regisztráció</h1>
-        <form action="" method="post" name="reg">
-            <label>Vezetéknév:</label>
-            <input type="text" name="vNev" required="" /><br><br> <!-- name="adatbázis adattag"-->
-            <label>Keresztnév:</label>
-            <input type="text" name="kNev" required="" /><br><br>
-            <label>Felhasználói név:</label>
-            <input type="text" name="nev" required="" /><br><br>
-            <label>Születési dátum:</label>
-            <input type="date" name="szulDatum" required="" /><br><br>
-            <label>Jelszó:</label>
-            <input type="password" name="jelszo" required="" /><br><br>
-            <label>Email cím:</label>
-            <input type="email" name="email" required="" /><br><br>
-            <input type="submit" name="submit" value="Regisztráció" /><br><br>
-            <a href="login.php">Belépéshez kattints ide</a>
-        </form>
-    </div>
-    <script src="delete.js"></script>
-</body>
-
+    </head>
+    <body>
+        <div id="container">
+            <h1>Regisztráció</h1>
+            <form action="" method="post" name="reg">
+                <label>Vezetéknév:</label>
+                <input type="text" name="vNev" required="" /><br><br>
+                <label>Keresztnév:</label>
+                <input type="text" name="kNev" required="" /><br><br>
+                <label>Születési dátum:</label>
+                <input type="date" name="szulDatum" required="" /><br><br>
+                <label>Felhasználói név:</label>
+                <input type="text" name="nev" required="" /><br><br>
+                <label>Email:</label>
+                <input type="text" name="email" required="" /><br><br>
+                <label>Password:</label>
+                <input type="password" name="jelszo" required="" /><br><br>
+                <input type="submit" name="submit" value="Regisztráció" /><br><br>
+                <a href="login.php">Belépéshez kattints ide</a>
+            </form>
+        </div>
+        <script src="delete.js"></script>
+    </body>
 </html>
